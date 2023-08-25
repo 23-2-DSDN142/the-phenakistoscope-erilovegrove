8cf43647d4e3111dccab43e30145419b86f10bc9
erilovegrove <140354030+erilovegrove@users.noreply.github.com>
2023-08-25 21:00:59 +1200

    Added temporary png's
    Quark and dimple png's

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,49 +1,50 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 10; //max and min slice count in brief
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("quark" , "png");
+  pScope.load_image("dimple" , "png");
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 0, 0, 0);  //lets us draw the whole circle background, ignoring the boundaries, basically the colour of circle
 
-  var layer1 = new PLayer(faces);
+  var layer1 = new PLayer(quarks); //this layer pertains to the faces function
   layer1.mode( SWIRL(5) );
   layer1.set_boundary( 200, 1000 );
 
-  var layer2 = new PLayer(squares);
+  var layer2 = new PLayer(dimples);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
+function quarks(x, y, animation, pScope){
   
   scale(animation.frame*2);
+  //scale (2);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  pScope.draw_image("quark",x,y);
 
 }
 
-function squares(x, y, animation, pScope){
+function dimples(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
+  fill(255, 255, 255)
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
+  fill(227, 104, 104)
   rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+
+  pScope.draw_image("dimple",-10,-300-animation.wave()*50);
 
 }
